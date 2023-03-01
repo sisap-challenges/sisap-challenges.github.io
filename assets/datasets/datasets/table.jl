7d2d11c54a4d1b34725be7bcc33ef59751a30dc0
sadit <donsadit@gmail.com>
2023-03-01 20:37:00 +0000

    Deploying to gh-pages from  @ 3391000cc89579b9676adeeda06a3453431fe814 🚀

--- assets/datasets/datasets/table.jl
+++ assets/datasets/datasets/table.jl
@@ -5,15 +5,18 @@ sizes = Dict()
 md5s = Dict()
 
 for line in readlines("dataset-files-urls.txt")
+    (length(line) == 0 || line[1] == '#') && continue
     urls[basename(line)] = line
 end
 
 for line in readlines("dataset-files-size.txt")
+    (length(line) == 0 || line[1] == '#') && continue
     s, name = split(line)
     sizes[basename(name)] = s
 end
 
 for line in readlines("dataset-files-md5.txt")
+    (length(line) == 0 || line[1] == '#') && continue
     s, name = split(line)
     md5s[basename(name)] = s
 end
@@ -24,6 +27,13 @@ function tablehead()
 end
 
 files = [
+  nothing => "768d clip embeddings",
+  "laion2B-en-clip768-n=100M.h5" => "100K subset",
+  "laion2B-en-clip768-n=30M.h5" => "100K subset",
+  "laion2B-en-clip768-n=10M.h5" => "100K subset",
+  "laion2B-en-clip768-n=300K.h5" => "100K subset, for developing purposes",
+  "laion2B-en-clip768-n=100K.h5" => "100K subset, for developing purposes",
+  "public-queries-10k-clip768.h5" => "10k public query set (original 768d embeddings)",
   nothing => "32d PCA projections",
   "laion2B-en-pca32-n=100M.h5" => "100M subset",
   "laion2B-en-pca32-n=30M.h5" => "30M subset",
@@ -45,8 +55,6 @@ files = [
   "laion2B-en-hamming-n=300K.h5" => "300K subset, for developing purposes",
   "laion2B-en-hamming-n=100K.h5" => "100K subset, for developing purposes",
   "public-queries-10k-hamming.h5" => "10k public query set for 1024-bit binary sketch projection",
-  nothing => "768d-embeddings public dataset",
-  "public-queries-10k.h5" => "10k public query set (original 768d embeddings)",
   nothing => "Gold standard list",
   "laion2B-en-public-gold-standard-100M.h5" => "100M gold standard",
   "laion2B-en-public-gold-standard-30M.h5" => "30M gold standard",
